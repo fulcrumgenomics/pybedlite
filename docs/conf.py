e3a2ac51fe780f3dc0f1c8ad64d6d856df6c18df
Nils Homer <nh13@users.noreply.github.com>
2025-03-13 11:19:41 -0700

    feat: remove python 3.8 and add support for 3.12 and 3.13 (#44)
    * feat: remove python 3.8 and add support for 3.12 and 3.13
    * chore: update actions versions to make CI work

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.napoleon']
 
-intersphinx_mapping = {'python': ('http://docs.python.org/3.8', None)}
+intersphinx_mapping = {'python': ('http://docs.python.org/3.9', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
